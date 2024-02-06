b2c7d5d25b5f0e9a85f5da99ef8b99ddefcf96ab
Lucas <lucasgabriellujan@hotmail.com>
2024-02-06 09:19:50 -0300

    consigna recuperatorio

--- practico_mvc_2/controller/habitacion.controller.php
+++ practico_mvc_2/controller/habitacion.controller.php
@@ -10,7 +10,20 @@ class Habitacion{
        $this->modelo = new HabitacionModel();
     }
     public function get_habitaciones(){
-        $habiaciones = $this->modelo->getHabitacionesModel();
-        $this->vista->listadoDeHabitaciones($habiaciones);
+        $habitaciones = $this->modelo->getHabitacionesModel();
+        $this->vista->listadoDeHabitaciones($habitaciones);
     }
 }
+function verify(){
+    if((empty($_POST['nro_habitacion']))||(empty($_POST['cant_camas']))||(empty($_POST['descripcion']))||(empty($_POST['ocupada']))){
+        $this->vista->listadoDeHabitaciones->showerror('faltan ingresar datos');
+
+    }
+}
+
+$nro_habitacion= $_POST['nro_habitacion'];
+$cant_camas= $_POST['cant_camas'];
+$descripcion= $_POST['descripcion'];
+$ocupada= $_POST ['ocupada'];
+
+
